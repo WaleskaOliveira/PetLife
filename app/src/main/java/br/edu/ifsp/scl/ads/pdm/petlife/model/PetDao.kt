245d82463b81391179d79923dfae6e5908ff2812
Waleska Oliveira <waleska.ziviani.peixoto@gmail.com>
2024-12-09 01:04:08 -0300

    feat: Criação da interface PetDao.kt

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/model/PetDao.kt
@@ -0,0 +1,9 @@
+package br.edu.ifsp.scl.ads.pdm.petlife.model
+
+interface PetDao  {
+    fun createPet(pet: Pet): Long
+    fun retriverPet(id: Int): Pet?
+    fun retriverPets(): MutableList<Pet>
+    fun updatePet(pet: Pet): Int
+    fun deletePet(id: Int): Int
+}
