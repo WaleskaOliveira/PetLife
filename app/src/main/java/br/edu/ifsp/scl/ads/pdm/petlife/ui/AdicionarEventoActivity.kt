9b1792d95e86ae3244006a243913650810baa3a1
Waleska Oliveira <waleska.ziviani.peixoto@gmail.com>
2024-12-09 09:36:59 -0300

    feat: Criação das classes AdicionarEventoActivity e activity_novo_evento.xml

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/ui/AdicionarEventoActivity.kt
@@ -0,0 +1,74 @@
+package br.edu.ifsp.scl.ads.pdm.petlife.ui
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.ads.pdm.petlife.databinding.ActivityNovoEventoBinding
+
+
+class AdicionarEventoActivity : AppCompatActivity() {
+    private val aevb: ActivityNovoEventoBinding by lazy {
+        ActivityNovoEventoBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(aevb.root)
+
+        val eventTypes = arrayOf("Last visit to the Vet", "Last visit to the Petshop", "Last vaccine")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, eventTypes)
+        aevb.eventTypeSp.adapter = adapter
+
+
+        val eventId = intent.getIntExtra("id", -1)
+        val eventType = intent.getStringExtra("eventType") ?: ""
+        val eventDate = intent.getStringExtra("eventDate") ?: ""
+        val eventDescription = intent.getStringExtra("eventDescription") ?: ""
+
+        if (eventId != -1) {
+            aevb.eventTypeSp.setSelection(adapter.getPosition(eventType))
+            aevb.eventDateEt.setText(eventDate)
+            aevb.eventDescriptionEt.setText(eventDescription)
+        }
+
+
+        aevb.saveEventBtn.setOnClickListener {
+            if (validateFields()) {
+                val eventTypeSelected = aevb.eventTypeSp.selectedItem.toString()
+                val eventDateInput = aevb.eventDateEt.text.toString()
+                val eventDescriptionInput = aevb.eventDescriptionEt.text.toString()
+
+
+                val resultIntent = intent.apply {
+                    putExtra("id", eventId)
+                    putExtra("eventType", eventTypeSelected)
+                    putExtra("eventDate", eventDateInput)
+                    putExtra("eventDescription", eventDescriptionInput)
+                }
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            }
+        }
+    }
+
+    // Valida os campos
+    private fun validateFields(): Boolean {
+        return when {
+            aevb.eventTypeSp.selectedItem == null -> {
+                showError("Please select the event type")
+                false
+            }
+            aevb.eventDateEt.text.isNullOrEmpty() -> {
+                showError("The date is obrigatory")
+                false
+            }
+
+            else -> true
+        }
+    }
+
+    private fun showError(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
