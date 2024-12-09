2141fd85f0d103eb74b7abdaa3cf6ae822650950
Waleska Oliveira <waleska.ziviani.peixoto@gmail.com>
2024-12-09 01:02:25 -0300

    feat: Criação da classe Pet.kt

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/petlife/model/Pet.kt
@@ -0,0 +1,14 @@
+package br.edu.ifsp.scl.ads.pdm.petlife.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Pet(
+    val ID: Int = 0,
+    val name: String = "",
+    val dateofbirth: String = "",
+    val type: String =  "",
+    val color: String =  "",
+    val size: String =  ""
+): Parcelable
